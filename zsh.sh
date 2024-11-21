2da473b19160cdfb1aa4eaf35d93922c9e85e721
MarioFronza <mariofronza1@gmail.com>
2024-11-21 12:59:58 -0300

    jetbrains toolbox

--- zsh.sh
+++ zsh.sh
@@ -13,7 +13,6 @@ chsh -s $(which zsh)
 # Install Powerlevel10k theme
 echo "Installing Powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' $HOME/.zshrc
 
 # Install recommended plugins
 echo "Installing recommended plugins..."
@@ -27,7 +26,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting $PLUGINS_DIR/zsh-
 
 # Update .zshrc to include plugins
 echo "Updating .zshrc to include plugins..."
-sed -i 's|plugins=(.*)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting)|' $HOME/.zshrc
 
 # Apply changes
 echo "Applying changes..."
